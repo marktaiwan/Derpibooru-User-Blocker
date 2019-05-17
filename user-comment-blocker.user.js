e942b7c7b36b4dcec71032d4728fe8c47394dc07
Mark Taiwan <mark.taiwan@gmail.com>
2019-05-17 11:07:50 +0800

    Version bump 0.4

--- user-comment-blocker.user.js
+++ user-comment-blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Marker's User Blocker
 // @description  Block people you don't want to see in the comments and forums
-// @version      0.3
+// @version      0.4
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
