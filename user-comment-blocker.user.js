c772e983a3d5da54c69a53ef5e66a941c6b56138
Mark Taiwan <marktaiwan@users.noreply.github.com>
2019-12-28 19:57:17 +0800

    Delete trailing spaces

--- user-comment-blocker.user.js
+++ user-comment-blocker.user.js
@@ -7,8 +7,8 @@
 // @namespace    https://github.com/marktaiwan/
 // @homepageURL  https://github.com/marktaiwan/Derpibooru-User-Blocker
 // @supportURL   https://github.com/marktaiwan/Derpibooru-User-Blocker/issues
-// @match        https://*.derpibooru.org/* 
-// @match        https://*.trixiebooru.org/* 
+// @match        https://*.derpibooru.org/*
+// @match        https://*.trixiebooru.org/*
 // @grant        none
 // @inject-into  content
 // @noframes
